f990a73a267d68c8fb82d118b40a9cdf84df59e8
iniya652007-ship-it <iniya652007@gmail.com>
2026-04-14 18:53:09 +0530

    Add files via upload

---
+++ MatrixToArray.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MatrixToArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] array = new int[rows * cols];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[index++] = matrix[i][j];
+            }
+        }
+
+        System.out.println("1D Array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
